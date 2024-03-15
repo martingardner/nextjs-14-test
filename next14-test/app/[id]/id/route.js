0454a495cfef18dce7d7a088bdae2fafc2b95f76
Martin Gardner II <martingardnerii@gmail.com>
2024-03-15 11:04:32 -0500

    update route

--- next14-test/app/[id]/id/route.js
+++ next14-test/app/[id]/id/route.js
@@ -1,3 +1,13 @@
+/**
+ * @function
+ * @param {Object} req 
+ * @param {{id:}} param1 
+ * @returns 
+ * String
+ * @description
+ * path to hit /{id}/id
+ */
+
 export async function GET(req, { params }){
     console.log('req', req)
     console.log('params', params);
