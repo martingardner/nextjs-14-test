b5bbcffee4abdabc7a5dec6c7845c2730a23b44a
Martin Gardner II <martingardnerii@gmail.com>
2024-03-16 13:45:18 -0500

    update to route/layout

--- next14-test/app/[id]/id/route.js
+++ next14-test/app/[id]/id/route.js
@@ -9,7 +9,6 @@
  */
 
 export async function GET(req, { params }){
-    //console.log('req', req)
-    //console.log('params', params);
+    console.log('params', params);
     return new Response(`GET Call for id ${params.id}`)
 }
--- next14-test/app/lizard/layout.js
+++ next14-test/app/lizard/layout.js
@@ -3,11 +3,13 @@ import Link from 'next/link';
 export default function DashboardLayout({ children }) {
     return (
         <section>
-        <nav>
-            <Link href="/lizard/raptor">Raptor</Link>
-            <Link href="/lizard/tyrant">Tyrant</Link>
-        </nav>
-        {children}
+            <nav>
+                <Link href="/lizard/raptor">Raptor</Link>
+                <Link href="/lizard/tyrant">Tyrant</Link>
+                <Link href="/lizard">Lizard</Link>
+                <Link href="/">Home</Link>
+            </nav>
+            {children}
         </section>
     )
 }
